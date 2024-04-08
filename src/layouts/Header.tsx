f94084d009155e8b223cd7f079c50e27473e582b
narvis2 <narvis2@douzone.com>
2024-04-08 16:35:16 +0900

    헤더 로고 아이콘 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -5,6 +5,8 @@ import { Button } from "@/components/ui/button";
 import useSpyElem from "@/hook/useSpyElem";
 import { Github } from "lucide-react";
 import Link from "next/link";
+import Logo from "../../public/favicon.ico";
+import Image from "next/image";
 
 const Header = () => {
   const { ref, marginTop } = useSpyElem(65);
@@ -17,10 +19,15 @@ const Header = () => {
     >
       <div className="mt-1 flex h-[64px] w-full max-w-[1200px] items-center justify-between px-4">
         <div className="flex items-center text-lg font-medium">
-          <Link href="/home">NarviS2</Link>
+          <Link href={"/home"} className="flex-row flex justify-center items-center">
+            <Image src={Logo} alt='로고' className="mx-auto object-cover rounded-full h-7 w-7 mr-2" />
+            <span className="text-lg font-medium text-gray-600 dark:text-white">
+              NarviS2
+            </span>
+          </Link>
         </div>
         <div className="flex gap-3">
-          <Button asChild variant="ghost" size="icon">
+          <Button asChild variant="outline" size="icon" className="rounded-[20px] outline-[#243c5a] items-center justify-center">
             <Link href="https://github.com/narvis2" target="_blank">
               <Github className="size-[1.2rem]" />
             </Link>
